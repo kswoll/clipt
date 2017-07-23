f28853e7e3f305ef79e11748c4953f7296eb361e
Kirk Woll <kirk.woll@gmail.com>
2017-07-23 13:43:28 -0700

    wip

--- Clipt/App.cs
+++ Clipt/App.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using Clipt.Actions;
 using Clipt.KeyboardHooks;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace Clipt
 {
     public class App : Application
     {
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WH_KEYBOARD_LL = 13;
+
         private static KeyboardHook keyboardHook;
+        private static TaskbarIcon notifyIcon;
+        private static IntPtr hookId;
+        private static LowLevelKeyboardProc proc = HookCallback;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
         /// <summary>
         /// Application Entry Point.
@@ -18,7 +43,16 @@ namespace Clipt
         public static void Main()
         {
             var app = new App();
-            app.Run();
+            hookId = SetHook(proc);
+
+            try
+            {
+                app.Run();
+            }
+            finally
+            {
+                UnhookWindowsHookEx(hookId);
+            }
         }
 
         protected override void OnStartup(StartupEventArgs e)
@@ -28,15 +62,53 @@ namespace Clipt
             keyboardHook = new KeyboardHook();
             keyboardHook.AddShortcut(ModifierKeys.Ctrl | ModifierKeys.Alt, KeyCode.V, () => new PasteAction().OnPaste());
 
-/*
-            GlobalStyles.RegisterStyles(Resources);
+            var logo = new BitmapImage();
+            logo.BeginInit();
+            logo.UriSource = new Uri("pack://application:,,,/Clipt;component/Assets/Wintomaton.ico");
+            logo.EndInit();
 
-            var mainWindow = new MainWindow
+            var contextMenu = new ContextMenu();
+            contextMenu.Items.Add(new MenuItem
             {
-                Model = new MainWindowModel()
+                Header = "Test"
+            });
+
+            notifyIcon = new TaskbarIcon
+            {
+                IconSource = logo,
+                ToolTipText = "Wintomaton",
+                ContextMenu = contextMenu
             };
-            mainWindow.Show();
-*/
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (var curProcess = Process.GetCurrentProcess())
+            using (var curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        /// <summary>
+        /// Return 1 to eat the keystroke.
+        /// </summary>
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                KeyCode vkCode = (KeyCode)Marshal.ReadInt32(lParam);
+
+                Console.WriteLine(vkCode);
+
+                if (vkCode == KeyCode.A)
+                {
+                    SendKey.SendKeyPress(KeyCode.D);
+                    return new IntPtr(1);
+                }
+            }
+
+            return CallNextHookEx(hookId, nCode, wParam, lParam);
         }
     }
 }
